e0728f2bd4f28baf78b7e0d5d535a6e8094ebad9
Richard Lam <54393902+richlamdev@users.noreply.github.com>
2019-12-30 20:34:11 -0800

    add setup files for kali xubuntu linux

---
+++ setup_kali_xubuntu.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# 
+# Basic setup script for Kali Linux VMware installation.
+# Created from Kali Linux 2019.4 - Xubuntu
+
+# Load default vimrc settings
+cp .vimrc ..
+
+# Install software
+apt update
+apt install ufw gobuster -y
+
+# Disable IPv6
+sed -e '/GRUB_CMDLINE_LINUX_DEFAULT=/ s/^#*/#/' -i_backup /etc/default/grub
+sed -e '/GRUB_CMDLINE_LINUX=/ s/^#*/#/' -i_backup /etc/default/grub
+sed -e '/GRUB_CMDLINE_LINUX_DEFAULT=/ a\GRUB_CMDLINE_LINUX_DEFAULT="quiet ipv6.disable=1"' -i_backup /etc/default/grub
+sed -e '/GRUB_CMDLINE_LINUX=/ a\GRUB_CMDLINE_LINUX="initrd=/install/initrd.gz ipv6.disable=1"' -i_backup /etc/default/grub
+update-grub2
+
+# Configure SSH Server
+sed -e '/AddressFamily/ s/^#*/#/' -i_backup /etc/ssh/sshd_config
+sed -e '/AddressFamily/ a\AddressFamily\ inet' -i_backup /etc/ssh/sshd_config
+sed -e '/X11Forwarding\ yes/ s/^#*/#/' -i_backup /etc/ssh/sshd_config
+sed -e '/X11Forwarding\ yes/ a\X11Forwarding\ no' -i_backup /etc/ssh/sshd_config
+sed -e '/PermitRootLogin\ prohibit-password/ s/^#*/#/' -i_backup /etc/ssh/sshd_config
+sed -e '/PermitRootLogin\ prohibit-password/ a\PermitRootLogin no' -i_backup /etc/ssh/sshd_config
+
+# Set SSH as only available service 
+#ufw limit 22/tcp
+# Default: no ports available
+ufw enable
+
+# Start OpenSSH Server
+#systemctl enable ssh
