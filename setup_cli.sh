ce2f878ece4be19f3e7db7271f88bb3467c062e7
Richard Lam <54393902+richlamdev@users.noreply.github.com>
2020-06-03 00:49:58 -0700

    minor change

--- setup_cli.sh
+++ setup_cli.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Quickly import vimrc and customize bash prompt PS1 environment variable.
+# import vimrc and customize bash prompt PS1 environment variable.
 
 cp .vimrc ~/.vimrc
 #echo 'export PS1="\A \[\e[31m\]\u\[\e[m\]@\[\e[36m\]\h\[\e[m\]:[\[\e[33m\]\w\[\e[m\]]\\$ "' >> ~/.bashrc
