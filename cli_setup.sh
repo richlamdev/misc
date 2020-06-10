be1e5cc08c0bbffd8fbb36f9e71df97671d38741
Richard Lam <54393902+richlamdev@users.noreply.github.com>
2020-06-10 14:53:28 -0700

    another change to PS1

--- cli_setup.sh
+++ cli_setup.sh
@@ -2,6 +2,6 @@
 # import vimrc and customize bash prompt PS1 environment variable.
 
 cp .vimrc ~/.vimrc
-export PS1="\A \[\e[36m\][\[\e[m\]\[\e[34m\]\u\[\e[m\]\[\e[34m\]@\[\e[m\]\[\e[31m\]\h\[\e[m\] \w\[\e[36m\]]\[\e[m\]\[\e[32m\]\\$\[\e[m\] "
+export PS1="\[\e[33m\]\A\[\e[m\] \[\e[36m\][\[\e[m\]\[\e[34m\]\u\[\e[m\]\[\e[34m\]@\[\e[m\]\[\e[31m\]\h\[\e[m\] \w\[\e[36m\]]\[\e[m\]\[\e[32m\]\\$\[\e[m\] "
 
 source ~/.bashrc
