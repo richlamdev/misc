5133915809b680bf542131cfcf174bd441f24d22
Richard Lam <54393902+richlamdev@users.noreply.github.com>
2020-06-17 21:09:40 -0700

    fix cli setup

--- cli_setup.sh
+++ cli_setup.sh
@@ -2,6 +2,6 @@
 # import vimrc and customize bash prompt PS1 environment variable.
 
 cp .vimrc ~/.vimrc
-export PS1="\[\033[38;5;214m\]\A\[$(tput sgr0)\] \[$(tput sgr0)\]\[\033[38;5;11m\][\[$(tput sgr0)\]\[\033[38;5;4m\]\u\[$(tput sgr0)\]@\[$(tput sgr0)\]\[\033[38;5;1m\]\h\[$(tput sgr0)\] \[$(tput sgr0)\]\[\033[38;5;14m\]\w\[$(tput sgr0)\]\[\033[38;5;11m\]]\\$\[$(tput sgr0)\] \[$(tput sgr0)\]"
+echo 'export PS1="\[\033[38;5;214m\]\A\[$(tput sgr0)\] \[$(tput sgr0)\]\[\033[38;5;11m\][\[$(tput sgr0)\]\[\033[38;5;4m\]\u\[$(tput sgr0)\]@\[$(tput sgr0)\]\[\033[38;5;1m\]\h\[$(tput sgr0)\] \[$(tput sgr0)\]\[\033[38;5;14m\]\w\[$(tput sgr0)\]\[\033[38;5;11m\]]\\$\[$(tput sgr0)\] \[$(tput sgr0)\]"' >> ~/.bashrc
 
 source ~/.bashrc
